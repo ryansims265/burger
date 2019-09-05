86e17e0d86cb349fa0298330757ec46fd80f90ab
Ryan Sims <ryan.sims@snapnurse.com>
2019-09-05 19:10:11 -0400

    Added code from previous activity for future tweaking

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,30 @@
-//Import orm.js
-
 //Call ORM functions
 
-//Export at the end of this code 
+var orm = require("../config/orm.js");
+
+var cat = {
+  all: function(cb) {
+    orm.all("cats", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("cats", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller 
+module.exports = cat;
